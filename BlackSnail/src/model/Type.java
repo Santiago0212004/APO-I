c634d86c996fae185348cbc7e896839276904f91
Santiago0212 <sjbs0212@gmail.com>
2021-11-06 17:15:06 -0500

    Explaining of "NONE" term in enums

--- BlackSnail/src/model/Type.java
+++ BlackSnail/src/model/Type.java
@@ -1,5 +1,7 @@
 package model;
 
+//"NONE" value is to initialize the variable when it is required, but at the end, this value is not used
+
 public enum Type {
 	NONE, NORMAL, GOLD, PLATINUM, DIAMOND; //All possible values of Type
 
