6fd906df8327bfe20a67734715de369ab21e23ad
Santiago Jose Barraza Sinning <1044607243@u.icesi.edu.co>
2021-11-18 09:51:30 -0500

    MatrixCalculator changes

--- MatrixCalculator/src/model/MatrixCalculator.java
+++ MatrixCalculator/src/model/MatrixCalculator.java
@@ -19,8 +19,24 @@ public class MatrixCalculator {
 		out += "Numeric matrices \n";
 		for(int i=0; i< numericMatrices.size(); i++) {
 			out+= printNumeric(numericMatrices.get(i));
+
+			if(hasOdd(numericMatrices.get(i))){
+				out += "\nHas at least one odd number";
+			}
+			else{
+				out += "\nDoesn't have odd numbers";
+			}
+
+			if(hasEven(numericMatrices.get(i))){
+				out += "\nHas at least one even number";
+			}
+			else{
+				out += "\nDoesn't have even numbers";
+			}
 		}
-		out += "String matrices \n";
+
+
+		out += "\n\nString matrices \n";
 		for(int i=0; i< stringMatrices.size(); i++) {
 			out+= printString(stringMatrices.get(i));
 		}
@@ -30,8 +46,12 @@ public class MatrixCalculator {
 
 	private String printString(String[][] matrix) {
 		String print ="";
-		
-
+		for (int i=0; i< matrix.length; i++ ) { // filas matrix.length
+			for (int j = 0; j < matrix[0].length; j++) { //columnas matrix[0].length
+				print += matrix[i][j] + " ";
+			}
+			print += "\n";
+		}
 		return print;
 	}
 
@@ -45,6 +65,7 @@ public class MatrixCalculator {
 		}
 		return print;
 	}
+
 	
 	
 
@@ -52,24 +73,99 @@ public class MatrixCalculator {
 		String out="";
 		switch(type) {
 		case 1: //numeric Matrix
-			int [][] newMatrix;
-			newMatrix= new int[rows][columns];
-			newMatrix[0][0] = 1;
-			numericMatrices.add(newMatrix);
-			out = printNumeric(newMatrix);
+			int [][] newIntMatrix;
+			newIntMatrix= new int[rows][columns];
+			
+			for(int i=0; i<rows; i++){
+				for(int j=0; j<columns; j++){
+					if(j==i){
+						newIntMatrix[i][j] = 1;
+					}
+					else{
+						newIntMatrix[i][j] = 0;
+					}
+				}
+			}
+
+			numericMatrices.add(newIntMatrix);
+			out = printNumeric(newIntMatrix);
 			
 			break;
 		case 2:
-			
+			String [][] newStringMatrix;
+			newStringMatrix = new String[rows][columns];
+
+			for(int i=0; i<rows; i++){
+				for(int j=0; j<columns; j++){
+					if(j==i){
+						newStringMatrix[i][j] = "A";
+					}
+					else{
+						newStringMatrix[i][j] = "E";
+					}
+				}
+			}
+
+			stringMatrices.add(newStringMatrix);
+			out = printString(newStringMatrix);
 		}
 		
 		return out;
-		
 	}
 	
-	public void addNumeric (int[][] numMatrix) {
+	public boolean hasOdd (int[][] numMatrix) {
+		boolean odd = false;
+
+		for (int i=0; i< numMatrix.length && !odd; i++ ) { 
+			for (int j = 0; j < numMatrix[0].length && !odd; j++) { 
+				if(numMatrix[i][j]%2==1){
+					odd = true;
+				}
+			}
+		}
+
+		return odd;
+	}
+
+	public boolean hasEven (int[][] numMatrix) {
+		boolean even = false;
+
+		for (int i=0; i< numMatrix.length && !even; i++ ) { 
+			for (int j = 0; j < numMatrix[0].length && !even; j++) { 
+				if(numMatrix[i][j]%2==0){
+					even = true;
+				}
+			}
+		}
+
+		return even;
+	}
+	
+	public boolean hasOddInDiffRows (int[][] numMatrix) {
+		boolean oddInRow = false;
+		boolean oddInMoreRows = false;
+
+		int odds = 0;
+
+
+		for (int i=0; i< numMatrix.length && !oddInMoreRows; i++ ) { 
+			for (int j = 0; j < numMatrix[0].length && !oddInRow; j++) { 
+				if(numMatrix[i][j]%2==1){
+					oddInRow = true;
+					odds += 1;
+
+					if(odds>1){
+						oddInMoreRows = true;
+					}
+				}
+			}
+		}
+
+		return oddInMoreRows;
+	}
+
+	public void addNumeric(int[][] numMatrix){
 		numericMatrices.add(numMatrix);
 	}
-	
 
 }
